962366077db396b863ca2fcbca710788740cb77b
jchibinin <chibinin@gmail.com>
2023-10-04 23:07:56 +0300

    Initial commit

---
+++ game_v3.py
@@ -0,0 +1,56 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    
+    # реализация методом бинарного поиска
+    low = -1
+    high = 101
+    while low<=high:
+        count += 1
+        predict_number = (low+high) // 2 # предполагаемое число
+        if predict_number==number:
+            break # выход из цикла если угадали
+        if predict_number>number:
+            high = predict_number - 1 # уменьшаем окно сверху
+        if predict_number<number:
+            low =  predict_number + 1  # уменьшаем окно сниху
+    
+    return count
+
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(game_core_v3)
